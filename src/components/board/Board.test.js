1ca3babe7b7d4a24208243ccef6898df07bc3eb4
iDmitryi <hdumitru1998@gmail.com>
2023-01-30 21:41:55 +0200

    prettier

--- src/components/board/Board.test.js
+++ src/components/board/Board.test.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Board from './Board';
+import Board from "./Board";
 
-describe('Board Component', () => {
-	it('renders without crashing', () =>{
-		const div = document.createElement('div');
-		ReactDOM.render(<Board />, div);
-	});
+describe("Board Component", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Board />, div);
+  });
 });
